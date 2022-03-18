7036c0b1de1dc1e4aef81fdd6bd14a6bcb4a0abb
Jairo Sotelo <jairoandres689@gmail.com>
2022-03-18 17:59:49 -0500

    Not comment1

--- src/app.js
+++ src/app.js
@@ -1,21 +1,12 @@
 const express = require('express');
 var app = express();
-
 var http = require('http').Server(app);
-
 const io = require('socket.io')(http);
-
-
 app.use(require('./routes/myzoom.routes'));
-
 app.use(express.static(__dirname + '/public'));
-
 io.on('connection', (socket) => {
 	socket.on('stream', (image) => {
-		//emitir el evento a todos los sockets(emisores)conectados
 		socket.broadcast.emit('stream', image);
 	});
 });
-
-
 module.exports = http;
