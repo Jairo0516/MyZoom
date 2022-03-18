40f8e3ddaabf43ddeb965b0b26633bc2c7bc5e8b
Jairo Sotelo <jairoandres689@gmail.com>
2022-03-18 17:22:48 -0500

    Coment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const http = require('./app');
+//Comentario
 http.listen(3000, () => {
 	console.log('Servidor en el puerto 3000 yes');
 });
--- src/routes/myzoom.routes.js
+++ src/routes/myzoom.routes.js
@@ -1,5 +1,9 @@
 const { Router } = require('express');
 const router = Router();
+
+
+//Comentario
+//Comentario
 router.get('/', (req, res) => {
 	res.redirect('index.html');
 });
